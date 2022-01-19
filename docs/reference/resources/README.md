fbe4835a6f9d0f26de28167a04b81390b8b02fb9
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2022-01-19 15:24:02 -0500

    feat: add cloud resource diagrams

---
+++ docs/reference/resources/README.md
@@ -0,0 +1,4 @@
+# Resources
+
+- [Amazon Web Services (AWS)](./aws.md)
+- [Google Cloud Platform (GCP)](./gcp.md)
---
+++ docs/reference/resources/img/export_model_images.py
@@ -0,0 +1,23 @@
+import requests
+
+core = "http://localhost:8900"
+kinds = requests.get(f"{core}/model").json()["kinds"]
+
+
+def export_images(path: str):
+    filter_out = ["aws", "gcp"]
+    for name, kind in kinds.items():
+        if [a for a in filter_out if name.startswith(f"{a}_")]:
+            image = requests.get(f"{core}/model/uml", params={"show": name})
+            with open(f"{path}/{name}.svg", "w+") as file:
+                file.write(image.text)
+
+
+def print_md(cloud: str):
+    for name in sorted(kinds):
+        if name.startswith(cloud):
+            print(f"## `{name}`\n")
+            print(f"![{name}](img/{name}.svg)\n")
+
+
+export_images(".")

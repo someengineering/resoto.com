f8941a5274cf4829e34940c9dd2faa860dc09acc
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2022-01-19 16:54:37 -0500

    feat: add GitHub stars

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,6 +21,14 @@ function HomepageHeader() {
           >
             View on GitHub
           </a>
+          <iframe
+            src="https://ghbtns.com/github-btn.html?user=someengineering&repo=resoto&type=star&count=true&size=large"
+            frameBorder="0"
+            scrolling="0"
+            width="170"
+            height="30"
+            title="GitHub Stars"
+          ></iframe>
         </div>
       </div>
     </header>

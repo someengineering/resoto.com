658d70d7654d805a417d036b60a9589eee0d127a
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2022-01-26 14:22:05 -0500

    fix: better detect dev environments

--- src/theme/Footer.tsx
+++ src/theme/Footer.tsx
@@ -1,22 +1,23 @@
-import BrowserOnly from '@docusaurus/BrowserOnly';
+import useIsBrowser from '@docusaurus/useIsBrowser';
 import OriginalFooter from '@theme-original/Footer';
 import React from 'react';
 
-export default process.env.NODE_ENV == 'development'
-  ? OriginalFooter
-  : function Footer(): JSX.Element {
-      return (
-        <>
-          <BrowserOnly>
-            {() => (
-              <img
-                src="https://static.scarf.sh/a.png?x-pxid=3b6ccd5c-8a2a-4bf3-94a3-e366b88342d8"
-                alt=""
-                style={{ height: '1px', width: '1px' }}
-              />
-            )}
-          </BrowserOnly>
-          <OriginalFooter />
-        </>
-      );
-    };
+export default function Footer(): JSX.Element {
+  const isBrowser = useIsBrowser();
+  const isDev =
+    process.env.NODE_ENV == 'development' ||
+    (isBrowser && window.location.href.includes('localhost'));
+
+  return isDev || !isBrowser ? (
+    <OriginalFooter />
+  ) : (
+    <>
+      <img
+        src="https://static.scarf.sh/a.png?x-pxid=3b6ccd5c-8a2a-4bf3-94a3-e366b88342d8"
+        alt=""
+        style={{ height: '1px', width: '1px' }}
+      />
+      <OriginalFooter />
+    </>
+  );
+}

01e9f13e11e81f616512ad266a20b2acaa5ab202
Lukas Lösche <lukas@some.engineering>
2022-04-21 18:40:50 +0200

    style(yarn format): yarn format

--- src/theme/CodeBlock/CopyButton/index.js
+++ src/theme/CodeBlock/CopyButton/index.js
@@ -51,9 +51,10 @@ export default function CopyButton({ code }) {
       className={clsx(
         styles.copyButton,
         'clean-btn',
-        isCopied && styles.copyButtonCopied,
+        isCopied && styles.copyButtonCopied
       )}
-      onClick={handleCopyCode}>
+      onClick={handleCopyCode}
+    >
       <span className={styles.copyButtonIcons} aria-hidden="true">
         <svg className={styles.copyButtonIcon} viewBox="0 0 24 24">
           <path d="M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z" />

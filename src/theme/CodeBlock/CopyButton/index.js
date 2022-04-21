2c1f5a76651a483b71b143c8c0b0159d59fcb4c0
Lukas Lösche <lukas@some.engineering>
2022-04-21 22:52:48 +0200

    fix(docs): add linefeed

--- src/theme/CodeBlock/CopyButton/index.js
+++ src/theme/CodeBlock/CopyButton/index.js
@@ -24,6 +24,7 @@ export default function CopyButton({ code }) {
         )
         .filter((str) => str)
         .join('\n')
+        .concat('\n')
     );
     setIsCopied(true);
     copyTimeout.current = window.setTimeout(() => {

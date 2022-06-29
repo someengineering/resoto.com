a3d6893fbf1a1c0a161dc8d29d9cf725ce64173d
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2022-06-29 08:26:17 -0700

    feat: proxy Plausible script and add opt-out page

---
+++ src/components/PlausibleExclude.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+
+export default function LatestRelease(): JSX.Element {
+  const [exclusionState, setExclusionState] = useState<boolean>(false);
+
+  useEffect(() => {
+    setExclusionState(
+      window.localStorage.getItem('plausible_ignore') === 'true'
+    );
+  }, []);
+
+  return (
+    <>
+      <p>
+        <strong>
+          You currently <em>{exclusionState ? 'are' : 'are not'}</em> excluding
+          your visits.
+        </strong>
+      </p>
+      <p>
+        <button
+          onClick={() => {
+            if (exclusionState) {
+              window.localStorage.removeItem('plausible_ignore');
+            } else {
+              window.localStorage.setItem('plausible_ignore', 'true');
+            }
+
+            setExclusionState(!exclusionState);
+          }}
+          className="button button--primary"
+        >
+          {exclusionState ? 'Stop Excluding My Visits' : 'Exclude My Visits'}
+        </button>
+      </p>
+    </>
+  );
+}
---
+++ src/pages/website-analytics.md
@@ -0,0 +1,15 @@
+# Website Analytics
+
+```mdx-code-block
+import PlausibleExclude from '@site/src/components/PlausibleExclude';
+```
+
+We track `resoto.com` website usage using [Plausible Analytics](https://plausible.io).
+
+[Plausible](https://plausible.io) tracks website usage without collecting personally identifiable information (PII) and without using cookies. [Plausible](https://plausible.io) complies with privacy regulations such as the GDPR, CCPA, and PECR. For details on how website usage data is handled, please refer to [Plausible's data policy](https://plausible.io/data-policy).
+
+## Opting Out
+
+To toggle whether your visits to `resoto.com` are counted, please click the button below.
+
+<PlausibleExclude />

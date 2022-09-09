511b278f8453f7dba0d2693ae71db89f926519cb
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2022-09-09 14:55:30 -0700

    refactor: wrap instead of ejecting DocVersionBadge

--- src/theme/DocVersionBadge/index.tsx
+++ src/theme/DocVersionBadge/index.tsx
@@ -1,31 +1,14 @@
-import { ThemeClassNames } from '@docusaurus/theme-common';
-import { useDocsVersion } from '@docusaurus/theme-common/internal';
-import Translate from '@docusaurus/Translate';
-import type { Props } from '@theme/DocVersionBadge';
-import clsx from 'clsx';
+import type { WrapperProps } from '@docusaurus/types';
+import DocVersionBadge from '@theme-original/DocVersionBadge';
+import type DocVersionBadgeType from '@theme/DocVersionBadge';
 import React from 'react';
 
-export default function DocVersionBadge({
-  className,
-}: Props): JSX.Element | null {
-  const versionMetadata = useDocsVersion();
-  if (versionMetadata.badge) {
-    return (
-      <span
-        className={clsx(
-          className,
-          ThemeClassNames.docs.docVersionBadge,
-          'badge badge--secondary sm-screens-only'
-        )}
-      >
-        <Translate
-          id="theme.docs.versionBadge.label"
-          values={{ versionLabel: versionMetadata.label }}
-        >
-          {'Version: {versionLabel}'}
-        </Translate>
-      </span>
-    );
-  }
-  return null;
+type Props = WrapperProps<typeof DocVersionBadgeType>;
+
+export default function DocVersionBadgeWrapper(props: Props): JSX.Element {
+  return (
+    <span className="sm-screens-only">
+      <DocVersionBadge {...props} />
+    </span>
+  );
 }

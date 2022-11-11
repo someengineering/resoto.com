9c7bb383cb67f988fe2129f0d4a7aba1c6e01b19
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2022-11-11 15:54:26 -0800

    feat(docs): sidebar method labels for API reference

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -104,7 +104,7 @@ const config = {
           remarkPlugins: [a11yEmoji],
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: [require.resolve('./src/css/custom.css'), require.resolve('./src/css/docusaurus-plugin-openapi-docs.css')],
         },
         sitemap: {
           changefreq: 'daily',

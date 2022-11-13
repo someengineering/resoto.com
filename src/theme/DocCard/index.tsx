88c2dcd0aabf575ce64bad8217814c9ff86305cb
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2022-11-12 19:21:42 -0800

    feat: show API method type in DocCards

---
+++ src/theme/DocCard/index.tsx
@@ -0,0 +1,122 @@
+import isInternalUrl from '@docusaurus/isInternalUrl';
+import Link from '@docusaurus/Link';
+import {
+  findFirstCategoryLink,
+  useDocById,
+} from '@docusaurus/theme-common/internal';
+import { translate } from '@docusaurus/Translate';
+import type { Props } from '@theme/DocCard';
+import clsx from 'clsx';
+import React, { type ReactNode } from 'react';
+
+import type {
+  PropSidebarItemCategory,
+  PropSidebarItemLink,
+} from '@docusaurus/plugin-content-docs';
+import styles from './styles.module.css';
+
+function CardContainer({
+  href,
+  children,
+  className,
+}: {
+  href: string;
+  children: ReactNode;
+  className?: string;
+}): JSX.Element {
+  return (
+    <Link
+      href={href}
+      className={clsx('card padding--lg', styles.cardContainer, className)}
+    >
+      {children}
+    </Link>
+  );
+}
+
+function CardLayout({
+  href,
+  icon,
+  title,
+  description,
+  className,
+}: {
+  href: string;
+  icon: ReactNode;
+  title: string;
+  description?: string;
+  className?: string;
+}): JSX.Element {
+  return (
+    <CardContainer href={href} className={className}>
+      <h2 className={clsx('text--truncate', styles.cardTitle)} title={title}>
+        {className?.includes('api-method') ? null : <>{icon} </>}
+        <span className="text--truncate">{title}</span>
+      </h2>
+      {description && (
+        <p
+          className={clsx('text--truncate', styles.cardDescription)}
+          title={description}
+        >
+          {description}
+        </p>
+      )}
+    </CardContainer>
+  );
+}
+
+function CardCategory({
+  item,
+}: {
+  item: PropSidebarItemCategory;
+}): JSX.Element | null {
+  const href = findFirstCategoryLink(item);
+
+  // Unexpected: categories that don't have a link have been filtered upfront
+  if (!href) {
+    return null;
+  }
+
+  return (
+    <CardLayout
+      href={href}
+      icon="🗃️"
+      title={item.label}
+      description={translate(
+        {
+          message: '{count} items',
+          id: 'theme.docs.DocCard.categoryDescription',
+          description:
+            'The default description for a category card in the generated index about how many items this category includes',
+        },
+        { count: item.items.length }
+      )}
+      className={item.className}
+    />
+  );
+}
+
+function CardLink({ item }: { item: PropSidebarItemLink }): JSX.Element {
+  const icon = isInternalUrl(item.href) ? '📄️' : '🔗';
+  const doc = useDocById(item.docId ?? undefined);
+  return (
+    <CardLayout
+      href={item.href}
+      icon={icon}
+      title={item.label}
+      description={doc?.description}
+      className={item.className}
+    />
+  );
+}
+
+export default function DocCard({ item }: Props): JSX.Element {
+  switch (item.type) {
+    case 'link':
+      return <CardLink item={item} />;
+    case 'category':
+      return <CardCategory item={item} />;
+    default:
+      throw new Error(`unknown item type ${JSON.stringify(item)}`);
+  }
+}

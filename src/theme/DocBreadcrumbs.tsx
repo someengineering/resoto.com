12b9ba60a8f8bbfcc76614d8ff0b6ac2656bb12d
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2023-01-03 13:47:28 -0800

    fix: resolve some GSC issues

--- src/theme/DocBreadcrumbs.tsx
+++ src/theme/DocBreadcrumbs.tsx
@@ -1,6 +1,5 @@
 import Link from '@docusaurus/Link';
 import { useSidebarBreadcrumbs } from '@docusaurus/theme-common/internal';
-import { translate } from '@docusaurus/Translate';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import React, { type ReactNode } from 'react';
 
@@ -11,13 +10,17 @@ function BreadcrumbsItemLink({
   children: ReactNode;
   href: string | undefined;
 }): JSX.Element {
-  const className = 'breadcrumbs__link';
   return href ? (
-    <Link href={href} itemProp="item" className={className}>
+    <Link className="breadcrumbs__link" href={href} itemProp="item">
       <span itemProp="name">{children}</span>
     </Link>
   ) : (
-    <span className={className}>{children}</span>
+    // TODO Google search console doesn't like breadcrumb items without href.
+    // The schema doesn't seem to require `id` for each `item`, although Google
+    // insist to infer one, even if it's invalid. Removing `itemProp="item
+    // name"` for now, since I don't know how to properly fix it.
+    // See https://github.com/facebook/docusaurus/issues/7241
+    <span className="breadcrumbs__link">{children}</span>
   );
 }
 
@@ -46,25 +49,6 @@ function BreadcrumbsItem({
   );
 }
 
-function HomeBreadcrumbItem() {
-  return (
-    <li className="breadcrumbs__item">
-      <Link
-        href={useBaseUrl('/docs')}
-        className="breadcrumbs__link"
-        aria-label={translate({
-          id: 'theme.docs.breadcrumbs.home',
-          message: 'Documentation',
-          description:
-            'The ARIA label for the docs home page in the breadcrumbs',
-        })}
-      >
-        Documentation
-      </Link>
-    </li>
-  );
-}
-
 export default function DocBreadcrumbs(): JSX.Element | null {
   const breadcrumbs = useSidebarBreadcrumbs();
 
@@ -74,20 +58,19 @@ export default function DocBreadcrumbs(): JSX.Element | null {
 
   breadcrumbs.pop();
 
+  breadcrumbs.unshift({
+    type: 'link',
+    href: useBaseUrl('/docs'),
+    label: 'Documentation',
+  });
+
   return (
-    <nav
-      aria-label={translate({
-        id: 'theme.docs.breadcrumbs.navAriaLabel',
-        message: 'Breadcrumbs',
-        description: 'The ARIA label for the breadcrumbs',
-      })}
-    >
+    <nav aria-label="Breadcrumbs">
       <ul
+        className="breadcrumbs breadcrumbs--sm"
         itemScope
         itemType="https://schema.org/BreadcrumbList"
-        className="breadcrumbs breadcrumbs--sm"
       >
-        <HomeBreadcrumbItem />
         {breadcrumbs.map((item, idx) => (
           <BreadcrumbsItem key={idx} index={idx} addMicrodata={!!item.href}>
             <BreadcrumbsItemLink href={item.href}>

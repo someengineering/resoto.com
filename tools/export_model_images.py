42fb454007c5f71d4646136d75d1d51ea23a5beb
Matthias Veit <matthias_veit@yahoo.de>
2023-01-25 10:19:59 +0100

    feat(docs): Improve exporter script

--- tools/export_model_images.py
+++ tools/export_model_images.py
@@ -1,15 +1,20 @@
 import os
 import os.path
-import sys
 import time
 from collections import defaultdict
+from itertools import takewhile
+from typing import Dict, List, Any
 
 import requests
 import urllib3
+from requests import Response
 from requests.adapters import HTTPAdapter
+from urllib3.exceptions import InsecureRequestWarning
 from urllib3.util import Retry
 
 core = "https://localhost:8900"
+
+# This list is used as filter: only providers listed here get exported
 providers = [
     "aws",
     "digitalocean",
@@ -24,26 +29,22 @@ providers = [
     "slack",
     "vsphere",
 ]
-required_providers = {"aws", "digitalocean", "gcp", "github", "kubernetes", "onelogin", "onprem", "slack", "vsphere"}
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+urllib3.disable_warnings(InsecureRequestWarning)
 
 
-def get_url(url: str, params: dict = None):
-    try:
-        session = requests.Session()
-        adapter = HTTPAdapter(
-            max_retries=Retry(
-                total=10,
-                backoff_factor=0.1,
-            )
+def get_url(url: str, params: dict = None) -> Response:
+    session = requests.Session()
+    adapter = HTTPAdapter(
+        max_retries=Retry(
+            total=3,
+            backoff_factor=0.1,
         )
-        session.mount("https://", adapter)
-        return session.get(url, params=params, verify=False)
-    except Exception:
-        return None
+    )
+    session.mount("https://", adapter)
+    return session.get(url, params=params, verify=False)
 
 
-def get_kinds():
+def get_kinds() -> Dict[str, List[Any]]:
     kinds = defaultdict(list)
     for kind in get_url(f"{core}/model").json():
         groups = [a for a in providers if kind["fqn"].startswith(f"{a}_") and kind.get("aggregate_root", False)]
@@ -76,62 +77,57 @@ def export_images(provider: str, kinds: list):
             file.write(image.text)
 
 
-def print_md(provider: str, kinds: list):
+def write_md(provider: str, kinds: list):
     if os.path.exists(f"./{provider}/index.md"):
+        # in case the file exists, read the header section until the first h2 (##)
         with (open(f"./{provider}/index.md", "r+")) as file:
-            lines = file.readlines()
+            lines = takewhile(lambda l: not l.startswith("## "), file.readlines())
     else:
+        # provider file does not exist, create default header
         lines = [
-            f"---\nsidebar_label: {provider.capitalize()}\n---",
-            "",
-            f"# {provider.capitalize()} Resource Data Models",
-            "",
-            "```mdx-code-block\nimport ZoomPanPinch from '@site/src/components/ZoomPanPinch';\n```",
+            f"---\nsidebar_label: {provider.capitalize()}\n---\n\n",
+            f"# {provider.capitalize()} Resource Data Models\n\n",
+            "```mdx-code-block\nimport ZoomPanPinch from '@site/src/components/ZoomPanPinch';\n```\n\n",
         ]
 
     with open(f"./{provider}/index.md", "w+") as file:
-        original_stdout = sys.stdout
-        sys.stdout = file
-
-        if ":::\n" in lines:
-            lastline = ":::"
-        else:
-            lastline = "```"
-
         for line in lines:
-            line = line.strip("\n")
-            print(line)
-
-            if line == lastline:
-                print()
-                break
+            file.write(line)
 
         for name in sorted(a["fqn"] for a in kinds):
-            print(f"## `{name}`\n")
-            print(f"<ZoomPanPinch>\n\n![Diagram of {name} data model](./img/{name}.svg)\n\n</ZoomPanPinch>\n")
-            print(f"<details>\n<summary>Relationships to Other Resources</summary>\n<div>\n<ZoomPanPinch>\n")
-            print(f"![Diagram of {name} resource relationships](./img/{name}_relationships.svg)\n")
-            print(f"</ZoomPanPinch>\n</div>\n</details>\n")
-
-        sys.stdout = original_stdout
+            file.write(f"## `{name}`\n\n")
+            file.write(f"<ZoomPanPinch>\n\n![Diagram of {name} data model](./img/{name}.svg)\n\n</ZoomPanPinch>\n\n")
+            file.write(f"<details>\n<summary>Relationships to Other Resources</summary>\n<div>\n<ZoomPanPinch>\n\n")
+            file.write(f"![Diagram of {name} resource relationships](./img/{name}_relationships.svg)\n\n")
+            file.write(f"</ZoomPanPinch>\n</div>\n</details>\n\n")
 
 
-kinds = get_kinds()
-retries = 0
-print(f"Kinds: {list(kinds.keys())}")
+def load_valid_kinds() -> Dict[str, Any]:
+    for _ in range(30): # nr of retries
+        try:
+            print("Get available kinds...")
+            kinds = get_kinds()
+            if kinds:
+                return kinds
+            else:
+                print(f"Missing some required kinds, trying again in 5 seconds...")
+        except Exception as ex:
+            print(f"Error getting list of kinds: {ex}")
+        time.sleep(5)
+    raise ValueError("Could not load kinds!")
 
-while retries < 60 and not required_providers.issubset(list(kinds.keys())):
-    print(f"Missing some required kinds, trying again in 5 seconds...")
-    time.sleep(5)
-    kinds = get_kinds()
-    print(f"New kinds: {list(kinds.keys())}")
-    retries += 1
 
-for provider in providers:
-    if len(kinds[provider]) > 0:
-        if not os.path.exists(f"./{provider}"):
-            os.mkdir(f"./{provider}")
-        if not os.path.exists(f"./{provider}/img"):
-            os.mkdir(f"./{provider}/img")
-        export_images(provider, kinds[provider])
-        print_md(provider, kinds[provider])
+def export() -> None:
+    kinds = load_valid_kinds()
+    for provider in providers:
+        if len(kinds.get(provider, [])) > 0:
+            if not os.path.exists(f"./{provider}"):
+                os.mkdir(f"./{provider}")
+            if not os.path.exists(f"./{provider}/img"):
+                os.mkdir(f"./{provider}/img")
+            print("---------------------------")
+            print(f"Create provider file: {provider} with {len(kinds[provider])} service kinds")
+            write_md(provider, kinds[provider])
+            export_images(provider, kinds[provider])
+
+export()

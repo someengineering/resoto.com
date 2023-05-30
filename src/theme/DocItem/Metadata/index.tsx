9ce0486ced679f15e084f72565a731a2c9e23af7
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2023-05-30 16:41:18 -0700

    feat(docs): automatically set install-method tab value

--- src/theme/DocItem/Metadata/index.tsx
+++ src/theme/DocItem/Metadata/index.tsx
@@ -1,10 +1,23 @@
 import { PageMetadata } from '@docusaurus/theme-common';
 import { useDoc } from '@docusaurus/theme-common/internal';
 import { getImage } from '@site/src/utils/socialImageHelper';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function DocItemMetadata(): JSX.Element {
   const { metadata, frontMatter, assets } = useDoc();
+
+  useEffect(() => {
+    if (
+      metadata.unversionedId.startsWith('getting-started/install-resoto') &&
+      metadata.unversionedId !== 'getting-started/install-resoto/index'
+    ) {
+      window.localStorage.setItem(
+        'docusaurus.tab.install-method',
+        metadata.unversionedId.split('/')[2]
+      );
+    }
+  }, []);
+
   return (
     <PageMetadata
       title={metadata.title}

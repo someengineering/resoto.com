a46f6b891240d0c72403a712369f881703544bb4
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2023-06-29 14:34:31 -0700

    feat(seo): add VideoObject structured data for YouTube embeds

--- src/components/YoutubeEmbed.tsx
+++ src/components/YoutubeEmbed.tsx
@@ -1,3 +1,4 @@
+import Head from '@docusaurus/Head';
 import clsx from 'clsx';
 import React from 'react';
 import LiteYouTubeEmbed from 'react-lite-youtube-embed';
@@ -7,22 +8,48 @@ type YoutubeEmbedProps = {
   id: string;
   title: string;
   className?: string;
+  date?: string;
 };
 
 export default function YoutubeEmbed({
   id,
   title,
   className,
+  date,
 }: YoutubeEmbedProps): JSX.Element {
+  title = title.replace(/^Episode\s\d+:\s/i, '');
+
   return (
-    <div className={clsx('video-container', className)}>
-      <LiteYouTubeEmbed
-        id={id}
-        title={title}
-        params="autoplay=1&rel=0"
-        poster="maxresdefault"
-        webp
-      />
-    </div>
+    <>
+      {date && (
+        <Head>
+          <script type="application/ld+json">
+            {JSON.stringify({
+              '@context': 'https://schema.org',
+              '@type': 'VideoObject',
+              name: title,
+              thumbnailUrl: `https://img.youtube.com/vi/${id}/maxresdefault.jpg`,
+              uploadDate: date.split('T', 1)[0],
+              contentUrl: `https://youtube.com/watch?v=${id}`,
+              embedUrl: `https://youtube.com/embed/${id}`,
+              publisher: {
+                '@type': 'Organization',
+                name: 'Some Engineering Inc.',
+                url: 'https://some.engineering',
+              },
+            })}
+          </script>
+        </Head>
+      )}
+      <div className={clsx('video-container', className)}>
+        <LiteYouTubeEmbed
+          id={id}
+          title={title}
+          params="autoplay=1&rel=0"
+          poster="maxresdefault"
+          webp
+        />
+      </div>
+    </>
   );
 }
